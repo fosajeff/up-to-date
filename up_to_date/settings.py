d044c19ea6a3291bb3efd498948917c7c9493757
Efosa Jeffrey <okoobohefosa@gmail.com>
2021-08-20 14:31:29 +0100

    fix 500 error

--- up_to_date/settings.py
+++ up_to_date/settings.py
@@ -125,12 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    Path.joinpath(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
