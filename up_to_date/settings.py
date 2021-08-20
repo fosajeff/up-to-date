f247914c5579f6994fa9d833c877fa7f6000f35c
Efosa Jeffrey <okoobohefosa@gmail.com>
2021-08-20 13:33:51 +0100

    Update ALLOWED_HOSTS with site and development server URL

--- up_to_date/settings.py
+++ up_to_date/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import dj_database_url
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x(=30nsk(s_p!h+jk_4i_w5ex813%uhz53scjt*+_tg!y4zsd+'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY', 'django-insecure')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["cpe500level.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
