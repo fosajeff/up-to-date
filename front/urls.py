86006e76395c62202615d01b69612d748334aa68
Efosa Jeffrey <okoobohefosa@gmail.com>
2021-08-23 01:18:19 +0100

    change admin title

--- front/urls.py
+++ front/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.dashboard)
 ]
+
+admin.site.site_header = 'CPE500 Level'
+admin.site.index_title = 'CPE500 Level Administration'
+admin.site.site_title = 'CPE500 Level Admin'
